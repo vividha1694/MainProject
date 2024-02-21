4676c7a3e5583a55600887604479699706b1c8de
vividha1694 <139147831+vividha1694@users.noreply.github.com>
2024-02-21 16:23:18 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
 
     <div>
       <Header/>
+      <Footer/>
       
     </div>
 
