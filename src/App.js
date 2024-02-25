5ea36278d3828ca5e9b42a2bc0a7bd144e72ed3d
vividha1694 <139147831+vividha1694@users.noreply.github.com>
2024-02-25 22:28:28 +0530

    added element

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer';
 import Background_img from './components/Home/Background_img';
-<<<<<<< HEAD
 import CategoryCardDetails from './components/Home/CategoryCardDetails';
 import CourseCardDetails from './components/Home/CourseCardDetails';
-
-=======
 import CounterUpPage from './components/Home/Counter/CounterUpPage';
 import Course_find from './components/Header/Course_find/Course_find';
->>>>>>> 53be2a57016e4db29921da59bb4d89e0a1f951dc
 
 
 const App = () => {
@@ -19,21 +15,13 @@ const App = () => {
     <div>
       <Header/>
       <Background_img/>
-<<<<<<< HEAD
       <CategoryCardDetails/>
       <CourseCardDetails/>
       <Footer/>
-=======
       <Home/>
       <CounterUpPage/>
       <Course_find/>
       <Footer/>
-      
-
-     
-
-      
->>>>>>> 53be2a57016e4db29921da59bb4d89e0a1f951dc
     </div>
 
   )
