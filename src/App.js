49c9808c0bb7284b4a31e388180f3937de1a72eb
vividha1694 <139147831+vividha1694@users.noreply.github.com>
2024-02-29 15:36:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import Home from './components/Home/Home'
+//import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer';
 import Background_img from './components/Home/Background_img';
 import CategoryCardDetails from './components/Home/CategoryCardDetails';
 import CourseCardDetails from './components/Home/CourseCardDetails';
-import CounterUpPage from './components/Home/Counter/CounterUpPage';
-import Course_find from './components/Header/Course_find/Course_find';
+//import CounterUpPage from './components/Home/Counter/CounterUpPage';
+//import Course_find from './components/Header/Course_find/Course_find';
 
 
 const App = () => {
@@ -16,9 +16,9 @@ const App = () => {
       <Header/>
       <Background_img/>
       <CategoryCardDetails/>
-      <CounterUpPage/>
+      {/* <CounterUpPage/> */}
       <CourseCardDetails/>
-      <Course_find/>
+      {/* <Course_find/> */}
       <Footer/>
     </div>
 
