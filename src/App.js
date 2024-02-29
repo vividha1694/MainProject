005dca14fb1b2c1736f832088d87aa9ab2cdb481
vividha1694 <139147831+vividha1694@users.noreply.github.com>
2024-02-29 16:29:05 +0530

    render elements

--- src/App.js
+++ src/App.js
@@ -12,12 +12,37 @@ import Course_find from './components/Header/Course_find/Course_find';
 import Playstore from './components/Home/PlayStore/Playstore';
 import Contact from './components/Contact/Contact';
 import Blog from './components/Blog/Blog';
+//import Brand from './components/About/Brand';
+//import TestimonialsCard from './components/About/TestimonialsCard';
+//import TestimonialsCardDetails from './components/About/TestimonialsCardDetails';
+//import About from './components/About/About';
+//import DiscoverResearch from './components/About/DiscoverResearch';
+//import TopInstructor from './components/About/TopInstructor';
+//import CourseCardDetails from './components/Courses/CourseCardDetails';
+//import GrayBackground from './components/GrayBackground/GrayBackground';
+//import VideoCard from './components/Home/VideoCard';
+//import Footer from './components/Footer/Footer';
+//import Home from './components/Home/Home';
+//import CourseCard from './components/Home/CourseCard';
+//import CourseCardDetails from './components/Home/CourseCardDetails';
 
 
 const App = () => {
   return (
 
     <div>
+     {/* <Brand/> */}
+    {/* <TestimonialsCardDetails/> */}
+    {/* <TopInstructor/> */}
+    {/* <DiscoverResearch/> */}
+    {/* <About/> */}
+    {/* <CourseCardDetails/> */}
+     {/* <GrayBackground/> */}
+    {/* <CourseCardDetails/> */}
+      {/* <VideoCard/> */}
+    {/* <Home/> */}
+     {/* <Footer/> */}
+
       {/* <Header/>*/}
       <Background_img/>
       <CategoryCardDetails/>
@@ -38,3 +63,6 @@ const App = () => {
 }
 
 export default App
+
+
+
